42625ff0c5a32dac93f0edacb1b243581780479b
Maykel-Lunch <157668149+Maykel-Lunch@users.noreply.github.com>
2025-04-16 11:08:14 +0800

    Cart views edit and modal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <script src="{{ asset('js/search.js') }}"></script>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Great+Vibes&family=Quicksand:wght@700&family=Poppins:wght@700&family=Montserrat:wght@700&family=Lora:wght@700&family=Dancing+Script:wght@700&family=Pacifico&family=Amatic+SC:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/wishlist.css') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <style>
         [x-cloak] { display: none !important; }
     </style>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -46,14 +46,15 @@
                         </a>
                     @endif
 
-
-                    <a href="/wishlist" class="text-white hover:text-gray-300 inline-flex flex-col items-center px-1 pt-1 text-xs font-medium"
-                        @click.prevent="isLoading = true; window.location.href = '/wishlist'">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 016.364 0L12 7.636l1.318-1.318a4.5 4.5 0 116.364 6.364L12 21.364l-7.682-7.682a4.5 4.5 0 010-6.364z" />
-                        </svg>
-                        <span>Wishlist</span>
-                    </a>
+                    @if (!Request::is('wishlist'))
+                        <a href="/wishlist" class="text-white hover:text-gray-300 inline-flex flex-col items-center px-1 pt-1 text-xs font-medium"
+                            @click.prevent="isLoading = true; window.location.href = '/wishlist'">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 016.364 0L12 7.636l1.318-1.318a4.5 4.5 0 116.364 6.364L12 21.364l-7.682-7.682a4.5 4.5 0 010-6.364z" />
+                            </svg>
+                            <span>Wishlist</span>
+                        </a>
+                    @endif
 
                     @if (!Request::is('cart'))
                         <a href="/cart" class="text-white hover:text-gray-300 inline-flex flex-col items-center px-1 pt-1 text-xs font-medium"
